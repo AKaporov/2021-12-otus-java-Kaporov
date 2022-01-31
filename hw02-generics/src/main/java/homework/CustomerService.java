76746152542d44f1c7abdc35f1116b4b2b48f216
AKaporov <voropak@yandex.ru>
2022-01-31 09:21:50 +0300

    исправление замечаний

--- hw02-generics/src/main/java/homework/CustomerService.java
+++ hw02-generics/src/main/java/homework/CustomerService.java
@@ -8,32 +8,28 @@ public class CustomerService {
     //todo: 3. надо реализовать методы этого класса
     //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
 
-    private SortedMap<Customer, String> map = new TreeMap<>(new Comparator<Customer>() {
-        @Override
-        public int compare(Customer o1, Customer o2) {
-            return (int) (o1.getScores() - o2.getScores());
-        }
-    });
-
+    private final NavigableMap<Customer, String> customers = new TreeMap<>(
+            Comparator.comparingLong(Customer::getScores)
+    );
 
     public Map.Entry<Customer, String> getSmallest() {
         //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
 
-        return getCustomerStringSimpleEntry(map.firstKey());
+        return getCustomerStringSimpleEntry(customers.firstKey());
     }
 
     private AbstractMap.SimpleEntry<Customer, String> getCustomerStringSimpleEntry(Customer customer) {
-        String value = map.get(customer);
+        String value = customers.get(customer);
         return new AbstractMap.SimpleEntry<>(new Customer(customer.getId(), customer.getName(), customer.getScores()), value);
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
 
-        if (map.containsKey(customer)) {
-            return getCustomerStringSimpleEntry(map.lastKey());
+        if (customers.containsKey(customer)) {
+            return getCustomerStringSimpleEntry(customers.lastKey());
         }
 
-        SortedMap<Customer, String> tailMap = map.tailMap(customer);
+        SortedMap<Customer, String> tailMap = customers.tailMap(customer);
         if (tailMap.isEmpty()) {
             return null;
         }
@@ -42,6 +38,6 @@ public class CustomerService {
     }
 
     public void add(Customer customer, String data) {
-        map.put(customer, data);
+        customers.put(customer, data);
     }
 }
