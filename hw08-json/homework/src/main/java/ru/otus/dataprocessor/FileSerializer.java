b5fef67194dac2571f576538ebcb6cc7f7925cc9
AKaporov <voropak@yandex.ru>
2022-03-17 16:26:37 +0300

    реализация

--- hw08-json/homework/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ hw08-json/homework/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -1,14 +1,30 @@
 package ru.otus.dataprocessor;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Map;
 
 public class FileSerializer implements Serializer {
+    private final String fileName;
 
     public FileSerializer(String fileName) {
+        this.fileName = fileName;
     }
 
     @Override
     public void serialize(Map<String, Double> data) {
         //формирует результирующий json и сохраняет его в файл
+
+        var gson = new Gson();
+        String json = gson.toJson(data);
+
+        try {
+            Files.writeString(Paths.get(fileName), json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
