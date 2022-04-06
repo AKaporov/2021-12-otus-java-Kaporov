97710fa913e4d2e961ee45c11887e74551475149
AKaporov <voropak@yandex.ru>
2022-04-06 21:00:45 +0300

    create branch and add new files

--- hw11-cache/src/main/java/ru/otus/cachehw/HwCache.java
+++ hw11-cache/src/main/java/ru/otus/cachehw/HwCache.java
@@ -1,8 +1,6 @@
 package ru.otus.cachehw;
 
 
-import java.util.List;
-
 public interface HwCache<K, V> {
 
     void put(K key, V value);
@@ -10,8 +8,6 @@ public interface HwCache<K, V> {
     void remove(K key);
 
     V get(K key);
-
-    List<V> getAll();
 
     void addListener(HwListener<K, V> listener);
 
