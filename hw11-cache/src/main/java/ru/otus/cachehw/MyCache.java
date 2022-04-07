1cd67a03f517a8880f28c0ac1e41e85101eeb794
AKaporov <voropak@yandex.ru>
2022-04-07 10:32:22 +0300

    реализация DbServiceClientImpl

--- hw11-cache/src/main/java/ru/otus/cachehw/MyCache.java
+++ hw11-cache/src/main/java/ru/otus/cachehw/MyCache.java
@@ -1,17 +1,21 @@
 package ru.otus.cachehw;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.WeakHashMap;
 
 public class MyCache<K, V> implements HwCache<K, V> {
 //Надо реализовать эти методы
 
-    Map<K, V> cache = new WeakHashMap<>();
+    private final Map<K, V> cache = new WeakHashMap<>();
+    private final List<HwListener<K, V>> listeners = new ArrayList<>();
+
 
     @Override
     public void put(K key, V value) {
-        cache.put(key, value);
+        cache.putIfAbsent(key, value);
     }
 
     @Override
@@ -25,12 +29,21 @@ public class MyCache<K, V> implements HwCache<K, V> {
     }
 
     @Override
-    public void addListener(HwListener<K, V> listener) {
+    public List<V> getAll() {
+        List<V> resultList = new ArrayList<>(cache.size());
 
+        cache.values().forEach(resultList::add);
+
+        return resultList;
+    }
+
+    @Override
+    public void addListener(HwListener<K, V> listener) {
+        listeners.add(listener);
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
-
+        listeners.remove(listener);
     }
 }
--- hw11-cache/src/main/java/ru/otus/service/DbServiceClientImpl.java
+++ hw11-cache/src/main/java/ru/otus/service/DbServiceClientImpl.java
@@ -2,6 +2,8 @@ package ru.otus.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.otus.cachehw.HwCache;
+import ru.otus.cachehw.MyCache;
 import ru.otus.core.repository.DataTemplate;
 import ru.otus.core.sessionmanager.TransactionRunner;
 import ru.otus.model.Client;
@@ -14,6 +16,7 @@ public class DbServiceClientImpl implements DBServiceClient {
 
     private final DataTemplate<Client> dataTemplate;
     private final TransactionRunner transactionRunner;
+    private final HwCache<String, Client> cache = new MyCache<>();
 
     public DbServiceClientImpl(TransactionRunner transactionRunner, DataTemplate<Client> dataTemplate) {
         this.transactionRunner = transactionRunner;
@@ -22,6 +25,14 @@ public class DbServiceClientImpl implements DBServiceClient {
 
     @Override
     public Client saveClient(Client client) {
+        Client savedClient = putClientInDB(client);
+
+        putClientInCache(savedClient);
+
+        return savedClient;
+    }
+
+    private Client putClientInDB(Client client) {
         return transactionRunner.doInTransaction(connection -> {
             if (client.getId() == null) {
                 var clientId = dataTemplate.insert(connection, client);
@@ -35,8 +46,20 @@ public class DbServiceClientImpl implements DBServiceClient {
         });
     }
 
+    private void putClientInCache(Client client) {
+        if (client != null) {
+            cache.put(getKey(client.getId()), client);
+        }
+    }
+
     @Override
     public Optional<Client> getClient(long id) {
+        Optional<Client> clientFoundInCache = getClientFromCache(id);
+
+        return clientFoundInCache.isPresent() ? clientFoundInCache : getClientFromDB(id);
+    }
+
+    private Optional<Client> getClientFromDB(long id) {
         return transactionRunner.doInTransaction(connection -> {
             var clientOptional = dataTemplate.findById(connection, id);
             log.info("client: {}", clientOptional);
@@ -44,12 +67,36 @@ public class DbServiceClientImpl implements DBServiceClient {
         });
     }
 
+    private Optional<Client> getClientFromCache(long id) {
+        Client clientFoundInCache = cache.get(getKey(id));
+
+        return clientFoundInCache == null ? null : Optional.of(clientFoundInCache);
+    }
+
+    private String getKey(long id) {
+        return "Client:".concat(String.valueOf(id));
+    }
+
     @Override
     public List<Client> findAll() {
+        List<Client> resultList = getAllClientsFromCache();
+
+        if (resultList.isEmpty()) {
+            return getAllClientsFromDB();
+        }
+
+        return resultList;
+    }
+
+    private List<Client> getAllClientsFromCache() {
+        return cache.getAll();
+    }
+
+    private List<Client> getAllClientsFromDB() {
         return transactionRunner.doInTransaction(connection -> {
             var clientList = dataTemplate.findAll(connection);
             log.info("clientList:{}", clientList);
             return clientList;
-       });
+        });
     }
 }
